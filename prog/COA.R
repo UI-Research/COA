3c500b84bcd9bc2d159f55c0500dfe853384bf9c
storresrod <storresrod@urban.org>
2021-10-21 16:14:13 -0600

    Added quarterly counts

--- prog/COA.R
+++ prog/COA.R
@@ -29,6 +29,8 @@ do.call("rbind", list)
 list.df <- do.call("rbind", lapply(list, as.data.frame)) 
 write.csv(list.df, file.path(path, "COA_monthbyzip.csv"))
 
+#Check - table(year2018$YYYYMM)
+
 #Make annual counts for each year (for destined, not originated, COAs)
 yearlycount_2018 <- year_2018 %>% 
   group_by(ZIPCODE) %>%
@@ -76,20 +78,143 @@ COA_yearly <- list1.df %>%
 write.csv(COA_yearly, file.path(path, "COA_yearlybyzip.csv"))
 
 #Report quarterly counts by zip code
-#Sort by zipcode to report all years together
-year_2018 %>% arrange(ZIPCODE) 
 
-#Attempt 1- Create month vector 1-12 - Issue: 29768*12 = 357216, but there's less rows than that in year_2018
-#month <- rep(c(1,2,3,4,5,6,7,8,9,10,11,12), times=29768)
-#year_2018$Month <- month
+#Pulling out the month - creating a column of quarters for 2018-2021
+quarter_2018 <- year_2018 %>%
+  mutate(Month=as.numeric(substring(YYYYMM, 5,6)), 
+         division = Month/3) 
+quarter_2018$Quarter <- ifelse(quarter_2018$division<=1, 1, 2)
+quarter_2018$Quarter <- ifelse(quarter_2018$division>2, 3, quarter_2018$Quarter)
+quarter_2018$Quarter <- ifelse(quarter_2018$division>3, 4, quarter_2018$Quarter)
 
-#Attempt 2 - Make a new column based off month in YYYYMM - Issue: Error in as.Date.default(., YYYYMM, format = "%Y%m%d") : do not know how to convert '.' to class "Date"
-#year_2018 %>% as.Date(YYYYMM, format = "%Y%m")
-#Group months by quarters some how
+quarter_2019 <- year_2019 %>%
+  mutate(Month=as.numeric(substring(YYYYMM, 5,6)), 
+         division = Month/3) 
+quarter_2019$Quarter <- ifelse(quarter_2019$division<=1, 1, 2)
+quarter_2019$Quarter <- ifelse(quarter_2019$division>2, 3, quarter_2019$Quarter)
+quarter_2019$Quarter <- ifelse(quarter_2019$division>3, 4, quarter_2019$Quarter)
 
-#Take these yearly, quarterly, monthly zipcode counts, and estimate for county level (crosswalk)
+quarter_2020 <- year_2020 %>%
+  mutate(Month=as.numeric(substring(YYYYMM, 5,6)), 
+         division = Month/3) 
+quarter_2020$Quarter <- ifelse(quarter_2020$division<=1, 1, 2)
+quarter_2020$Quarter <- ifelse(quarter_2020$division>2, 3, quarter_2020$Quarter)
+quarter_2020$Quarter <- ifelse(quarter_2020$division>3, 4, quarter_2020$Quarter)
+
+quarter_2021 <- year_2021 %>%
+  mutate(Month=as.numeric(substring(YYYYMM, 5,6)), 
+         division = Month/3) 
+quarter_2021$Quarter <- ifelse(quarter_2021$division<=1, 1, 2)
+quarter_2021$Quarter <- ifelse(quarter_2021$division>2, 3, quarter_2021$Quarter)
+quarter_2021$Quarter <- ifelse(quarter_2021$division>3, 4, quarter_2021$Quarter)
+
+#Creating quarter sums for 2018
+quartercount_2018 <- quarter_2018 %>% 
+  group_by(Quarter, ZIPCODE) %>%
+  summarise(sum_total = sum(TOTAL_FROM_ZIP),
+            sum_business = sum(TOTAL_BUSINESS),
+            sum_family = sum(TOTAL_FAMILY),
+            sum_individual = sum(TOTAL_INDIVIDUAL),
+            sum_perm = sum(TOTAL_PERM),
+            sum_temp = sum(TOTAL_TEMP)) 
+quartercount_2018$Year <- "2018"
+
+quartercount_2019 <- quarter_2019 %>% 
+  group_by(Quarter, ZIPCODE) %>%
+  summarise(sum_total = sum(TOTAL_FROM_ZIP),
+            sum_business = sum(TOTAL_BUSINESS),
+            sum_family = sum(TOTAL_FAMILY),
+            sum_individual = sum(TOTAL_INDIVIDUAL),
+            sum_perm = sum(TOTAL_PERM),
+            sum_temp = sum(TOTAL_TEMP)) 
+quartercount_2019$Year <- "2019"
+
+quartercount_2020 <- quarter_2020 %>% 
+  group_by(Quarter, ZIPCODE) %>%
+  summarise(sum_total = sum(TOTAL_FROM_ZIP),
+            sum_business = sum(TOTAL_BUSINESS),
+            sum_family = sum(TOTAL_FAMILY),
+            sum_individual = sum(TOTAL_INDIVIDUAL),
+            sum_perm = sum(TOTAL_PERM),
+            sum_temp = sum(TOTAL_TEMP)) 
+quartercount_2020$Year <- "2020"
+
+quartercount_2021 <- quarter_2021 %>% 
+  group_by(Quarter, ZIPCODE) %>%
+  summarise(sum_total = sum(TOTAL_FROM_ZIP),
+            sum_business = sum(TOTAL_BUSINESS),
+            sum_family = sum(TOTAL_FAMILY),
+            sum_individual = sum(TOTAL_INDIVIDUAL),
+            sum_perm = sum(TOTAL_PERM),
+            sum_temp = sum(TOTAL_TEMP)) 
+quartercount_2021$Year <- "2021"
+
+# Sum the quarter amounts across 2018-2021 
+list2 <- list(quartercount_2018,quartercount_2019,quartercount_2020,quartercount_2021)
+do.call("rbind", list2)
+list2.df <- do.call("rbind", lapply(list2, as.data.frame)) 
+write.csv(list2.df, file.path(path, "check.csv"))
+COA_quarterly <- list2.df %>% 
+  group_by(Year,Quarter,ZIPCODE) %>%
+  summarise(total = sum(sum_total),
+            business = sum(sum_business),
+            family = sum(sum_family),
+            individual = sum(sum_individual),
+            perm = sum(sum_perm),
+            temp = sum(sum_temp))
+write.csv(COA_quarterly, file.path(path, "COA_quarterlybyzip.csv"))
+
+##########################################################################################
+#Take zipcode estimates to county level (crosswalk and spatial join)
+library(tigris)
+library(ggplot2)
+library(urbnmapr)
+library(urbnthemes)
+library(sf)
+set_urbn_defaults(style = "map")
+
+#Pull zcta shape files - make an sf object
+zcta_2018 <- zctas(cb = FALSE, starts_with = NULL, year = 2018, state = NULL)
+zcta_2018<- st_as_sf(zcta_2018)
+
+zcta_2019 <-zctas(cb = FALSE, starts_with = NULL, year = 2019, state = NULL)
+zcta_2019<- st_as_sf(zcta_2019)
+
+zcta_2020 <-zctas(cb = FALSE, starts_with = NULL, year = 2020, state = NULL)
+zcta_2020<- st_as_sf(zcta_2020)
+
+zcta_2021 <-zctas(cb = FALSE, starts_with = NULL, year = 2021, state = NULL)
+zcta_2021<- st_as_sf(zcta_2021)
+
+#Pull county shape files - make into sf objects
+counties_2018 <-counties(cb = FALSE, year = 2018, state = NULL) 
+counties_2018<- st_as_sf(counties_2018)
+
+counties_2019 <-counties(cb = FALSE, year = 2019, state = NULL) 
+counties_2019<- st_as_sf(counties_2019)
+
+counties_2020 <-counties(cb = FALSE, year = 2020, state = NULL)
+counties_2020<- st_as_sf(counties_2020)
+
+counties_2021 <-counties(cb = FALSE, year = 2021, state = NULL) 
+counties_2021<- st_as_sf(counties_2021)
+
+#Left join usps data to zcta shape file
+rename(year_2018, ZCTA5CE10=ZIPCODE)
+rename(year_2019, ZCTA5CE10=ZIPCODE)
+rename(year_2020, ZCTA5CE10=ZIPCODE)
+rename(year_2021, ZCTA5CE10=ZIPCODE)
+
+zctajoin_2018 <- st_join(year_2018, zcta_2018, join = st_intersects)
+
+#step 4 - spatial joining joined zcta to counties 
+
+#step 5 - group by county, summarize key counts - aggregation?
+
+#pivot wider function - get everything in one place and break into different columns for months and quarters 
 
 #Get denominator of addresses by zipcode and county, create rates of mobility
+#tidy census total households by zzta
 
 #Validate with other data
 
